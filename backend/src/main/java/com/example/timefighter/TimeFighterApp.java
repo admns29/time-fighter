78c85f18cb79798c2dd184a3df4c8071d118689d
MUHAMMAD ADAM MOHD NASIR <66550590+admns29@users.noreply.github.com>
2025-10-26 20:37:22 +0800

    Initial commit

---
+++ backend/src/main/java/com/example/timefighter/TimeFighterApp.java
@@ -0,0 +1,11 @@
+package com.example.timefighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFighterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeFighterApp.class, args);
+    }
+}
